c19f6bee3d48071c0041ae9d2395b8c9a4706200
Laura Marques <vitoria.lauuraa@gmail.com>
2026-09-11 10:19:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const gerarBtn = document.getElementById('gerar-btn');
+const logo = document.querySelector('.logo')
+logo.style.display = "none";
+
 
 gerarBtn.addEventListener('click', () => {
     const nome = document.getElementById('nome').value;
@@ -6,11 +9,36 @@ gerarBtn.addEventListener('click', () => {
     const userGit = document.getElementById('username').value;
 
 
+
     if (nome == "" || email == "" || userGit == "") {
         alert("Complete the required fields!");
+
+
     } else {
+        gerarBtn.style.display = "none";
+        logo.style.display = "flex";
+
         const titulo = document.querySelector('h1');
+        const subtitulo = document.querySelector('h2');
+        const ticketImagem = document.createElement('img');
+        const form = document.querySelector('form');
+
+
         titulo.textContent = `Congrats, ${nome}! Your ticket is ready!`;
+        subtitulo.textContent = `We've emailed your ticket to ${email} and will send updates in the run up to the event.`;
+        ticketImagem.setAttribute('src', 'assets/images/pattern-ticket.svg');
+        form.classList.add('ticket');
+
+        const dadosEvent = document.createElement('p');
+        dadosEvent.textContent = "Jan 31, 2025 / Austin, TX";
+        form.append(dadosEvent)
+
+        const novoNome = document.createElement('p');
+        novoNome.textContent = nome;
+        novoNome.classList.add('nome-alterado')
+        form.append(novoNome)
+
+
 
     }
 })
