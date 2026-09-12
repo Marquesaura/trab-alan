5ba2c864106dbcdd28d872e0d2463f83b2534a2e
Laura Marques <vitoria.lauuraa@gmail.com>
2026-09-11 22:14:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,38 +16,53 @@ gerarBtn.addEventListener('click', () => {
     } else {
         gerarBtn.style.display = 'none';
         logo.style.display = 'flex';
-        
+
 
         const titulo = document.querySelector('h1');
         const subtitulo = document.querySelector('h2');
         const ticketImagem = document.createElement('img');
         ticketImagem.setAttribute('src', 'assets/images/pattern-ticket.svg');
         const form = document.querySelector('form');
-        
-        const span = document.querySelector('span');
-        
 
+        const span = document.querySelector('span');
+
+        subtitulo.style.display = 'none';
 
         titulo.style.display = 'none';
 
-        const mensagem = document.createElement('h1');
-        mensagem.textContent = 'Congrats';
-        mensagem.classList.add('mensagem-ticket');
-        span.append(mensagem);
+        const parabens = document.createElement('h1');
+        parabens.textContent = 'Congrats,';
+        parabens.classList.add('mensagem-inicial');
+        span.append(parabens);
+        parabens.classList.add = ('parabens')
 
         const novoNome = document.createElement('h1');
-        novoNome.textContent = nome;
+        novoNome.textContent = `${nome}!`;
         novoNome.classList.add('nome-alterado');
         span.append(novoNome);
 
         const resto = document.createElement('h1');
-        resto.textContent = '! Your ticket is ready!';
-        resto.classList.add('mensagem-ticket');
+        resto.textContent = 'Your ticket is ready!';
+        resto.classList.add('mensagem-inicial');
         span.append(resto);
 
-        subtitulo.textContent = `We've emailed your ticket to ${email} and will send updates in the run up to the event.`;
+        const mensagem = document.createElement('p');
+        mensagem.textContent = "We've emailed your ticket to";
+        mensagem.classList.add('mensagem-ticket');
+        span.append(mensagem);
+
+        const novoEmail = document.createElement('p');
+        novoEmail.textContent = email;
+        novoEmail.classList.add('nome-alterado');
+        span.append(novoEmail);
+
+        const restoMensagem = document.createElement('p');
+        restoMensagem.textContent = "and will send updates in the run up to the event.";
+        restoMensagem.classList.add('mensagem-ticket');
+        span.append(restoMensagem);
 
         form.classList.add('ticket');
+        span.classList.add('novo-estilo')
 
         const dadosEvent = document.createElement('p');
         dadosEvent.textContent = 'Jan 31, 2025 / Austin, TX';
